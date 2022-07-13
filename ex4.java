ee306bcdff856ada9495ad9c75aa8d4c6fee85dd
s1290189 <109191541+s1290189@users.noreply.github.com>
2022-07-13 12:14:24 +0900

    Create ex4.java
    HoT: initial revision

---
+++ ex4.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+	    int r, heads=0, tails=0;
+		System.out.println("Tossing a coin...");
+		for(int i=1; i<=3; i++){
+		    r=new java.util.Random().nextInt(2);
+		    if(r==0){
+		        System.out.println("Round "+i+": Heads");
+		        heads++;
+		    }
+		    else{
+		        System.out.println("Round "+i+": Tails");
+		        tails++;
+		    }
+		}
+		System.out.println("Heads: "+heads+", Tails: "+tails);
+	}
+}
